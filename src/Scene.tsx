7659ef0ff89eb67089fd4a88e6d0f47ffce533f1
gimleng <nawapon.nuang@gmail.com>
2026-05-06 12:31:49 +0700

    fix performance

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,31 +1,37 @@
 // @ts-expect-error - PlayCanvas ESM scripts don't have type declarations
 import { CameraControls } from "playcanvas/scripts/esm/camera-controls.mjs";
-// @ts-expect-error - PlayCanvas ESM scripts don't have type declarations
-import { Grid } from "playcanvas/scripts/esm/grid.mjs";
 import { Entity } from "@playcanvas/react";
 import { Camera, Script, GSplat } from "@playcanvas/react/components";
 import { useSplat } from "@playcanvas/react/hooks";
 
+function SkyVIVOffice() {
+  const { asset } = useSplat("skyviv-office.sog");
+  if (!asset) return null;
+
+  return (
+    <Entity position={[0, 0, 0]} rotation={[0, 0, 180]}>
+      <GSplat
+        asset={asset}
+        // Aggressive LOD: drop quality at 2.5m, drop fast as distance grows
+        lodBaseDistance={2.5}
+        lodMultiplier={4}
+      />
+    </Entity>
+  );
+}
+
 function Scene() {
-  
-  function SkyVIVOffice() {
-    const { asset } = useSplat("skyviv-office.sog");
-    if (!asset) return null;
-
-    return (
-      <Entity position={[0, 0, 0]} rotation={[0, 0, 180]}>
-        <GSplat highQualitySH={true}  asset={asset} />
-      </Entity>
-    );
-  }
-
   return (
     <>
       <Entity name="camera" position={[-8.5, 2, 8]}>
-        <Camera clearColor="#171717" />
+        <Camera
+          clearColor="#171717"
+          farClip={50}
+          nearClip={0.1}
+          fov={60}
+        />
         <Script script={CameraControls} />
       </Entity>
-
       <SkyVIVOffice />
     </>
   );
